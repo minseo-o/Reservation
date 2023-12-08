75933352c316ce1bb9baa4cd06b62452afe16dd3
minseo-o <halia0709@gmail.com>
2023-12-08 13:57:11 +0900

    test

--- index.php
+++ index.php
@@ -4,14 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple PHP Board</title>
+    <title>Simple PHP Board with Image Upload</title>
 </head>
 
 <body>
 
     <?php
-    // 데이터베이스 연결 설정
-    include('./db_conn.php');
+   include('./db_conn.php');
 
     // 게시물 불러오기
     $sql = "SELECT id, title, content, image_path, created_at FROM posts ORDER BY created_at DESC";
@@ -23,27 +22,32 @@
             echo "<div>";
             echo "<h2>" . $row["title"] . "</h2>";
             echo "<p>" . $row["content"] . "</p>";
+            
+            // 이미지 출력
             if (!empty($row["image_path"])) {
-                echo '<img src="' . $row["image_path"] . '" alt="Post Image">';
+                echo '<img src="' . $row["image_path"] . '" alt="Post Image" style="max-width: 100%;">';
             }
+            
             echo "<p>Created at: " . $row["created_at"] . "</p>";
             echo "</div>";
         }
     } else {
         echo "No posts yet.";
     }
-    // 이미지 출력
-if (!empty($row["image_path"])) {
-    echo '<img src="' . $row["image_path"] . '" alt="Post Image" style="max-width: 100%;">';
-}
 
+    // 이미지 출력
+if (!empty($row["image_path"]) && file_exists($row["image_path"])) {
+    echo '<img src="' . $row["image_path"] . '" alt="Post Image" style="max-width: 100%;">';
+} else {
+    echo 'No image available.';
+}
 
     // 데이터베이스 연결 종료
     $conn->close();
     ?>
 
     <!-- 게시물 작성 폼 -->
-    <form action="submit_post.php" method="post">
+    <form action="submit_post.php" method="post" enctype="multipart/form-data">
         <label for="title">Title:</label>
         <input type="text" name="title" required>
         <br>
--- join.php
+++ join.php
@@ -3,15 +3,16 @@
 include('./db_conn.php');
 
 try {
- 
+    
     $name = $_POST["uname"];
     $id = $_POST["uid"];
     $password = $_POST["upassword"];
     $mail = $_POST["uemail"];
     $mobile = $_POST["umobile"];
+    $gender = $_POST["gender"];
 
     // 사용자 정보를 데이터베이스에 삽입하는 SQL 쿼리
-    $sql = "INSERT INTO member  VALUES ( '$id', '$password','$name', '$mail','$mobile')";
+    $sql = "INSERT INTO member  VALUES ( '$id', '$password','$name', '$mail','$mobile' ,'$gender')";
 
     // SQL 쿼리 실행
     if ($conn->query($sql) === TRUE) {
